8cbc4d928f4c30c53420bb3b07c0a07e4190f17b
LeonitKrasniqi <leonitkr88@gmail.com>
2024-05-08 18:19:21 +0200

    Included JpaRepository in AccountRepository.

--- src/main/java/repository/AccountRepository.java
+++ src/main/java/repository/AccountRepository.java
@@ -1,7 +1,6 @@
 package repository;
 
 import model.Account;
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
